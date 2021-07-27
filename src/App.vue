9764189ebe806476047da94b11292b65ad0f50eb
fujinting <2239216274@qq.com>
2021-07-27 13:28:35 +0800

    初始化仓库

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 默认 -->
+    <div class="button">
+      <div>默认按钮</div>
+      <fu-button>默认按钮</fu-button>
+      <fu-button type="primary">主要按钮</fu-button>
+      <fu-button type="success">成功按钮</fu-button>
+      <fu-button type="info">信息按钮</fu-button>
+      <fu-button type="warning">警告按钮</fu-button>
+      <fu-button type="danger">危险按钮</fu-button>
+    </div>
+
+    <!-- 朴素 -->
+    <div class="button">
+      <div>朴素按钮</div>
+      <fu-button plain>默认按钮</fu-button>
+      <fu-button type="primary" plain>主要按钮</fu-button>
+      <fu-button type="success" plain>成功按钮</fu-button>
+      <fu-button type="info" plain>信息按钮</fu-button>
+      <fu-button type="warning" plain>警告按钮</fu-button>
+      <fu-button type="danger" plain>危险按钮</fu-button>
+    </div>
+
+    <!-- 圆角 -->
+    <div class="button">
+      <div>圆角按钮</div>
+      <fu-button round>默认按钮</fu-button>
+      <fu-button type="primary" round>主要按钮</fu-button>
+      <fu-button type="success" round>成功按钮</fu-button>
+      <fu-button type="info" round>信息按钮</fu-button>
+      <fu-button type="warning" round>警告按钮</fu-button>
+      <fu-button type="danger" round>危险按钮</fu-button>
+    </div>
+
+    <!-- 圆圈 -->
+    <div class="button">
+      <div>圆型按钮</div>
+      <fu-button icon="fu-icon-search" circle></fu-button>
+      <fu-button icon="fu-icon-edit" type="primary" circle></fu-button>
+      <fu-button icon="fu-icon-check" type="success" circle></fu-button>
+      <fu-button icon="fu-icon-message" type="info" circle></fu-button>
+      <fu-button icon="fu-icon-star" type="warning" circle></fu-button>
+      <fu-button icon="fu-icon-delete" type="danger" circle></fu-button>
+    </div>
+
+    <!-- 禁用 -->
+    <div class="button">
+      <div>禁用按钮</div>
+      <fu-button disabled>默认按钮</fu-button>
+      <fu-button type="primary" disabled>主要按钮</fu-button>
+      <fu-button type="success" disabled>成功按钮</fu-button>
+      <fu-button type="info" disabled>信息按钮</fu-button>
+      <fu-button type="warning" disabled>警告按钮</fu-button>
+      <fu-button type="danger" disabled>危险按钮</fu-button>
+    </div>
+
+    <!-- 带图标 -->
+    <div class="button">
+      <div>图标按钮</div>
+      <fu-button icon="fu-icon-search">默认按钮</fu-button>
+      <fu-button icon="fu-icon-edit" type="primary">主要按钮</fu-button>
+      <fu-button icon="fu-icon-check" type="success">成功按钮</fu-button>
+      <fu-button icon="fu-icon-message" type="info">信息按钮</fu-button>
+      <fu-button icon="fu-icon-star" type="warning">警告按钮</fu-button>
+      <fu-button icon="fu-icon-delete" type="danger">危险按钮</fu-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .button {
+    width: 45%;
+    margin: 30px auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FuButton from './packages/button.vue'
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
+Vue.component(FuButton.name, FuButton)
 
 new Vue({
   render: h => h(App)
